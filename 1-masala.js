741be1fb0cb7b5d471289a2b63034e6467171eef
Khakimov Mukhammadaziz <khakimovmukhammadaziz@gmail.com>
2025-01-30 18:26:23 +0500

    first commit

---
+++ 1-masala.js
@@ -0,0 +1,21 @@
+// let a = 25; //number
+// let str = "hello world"; // string
+// let boolean = true; // boolean
+// let obj = {}; // object
+// let person = undefined; // undefined
+// let human = null; //null
+// let array = []; //array
+// let funct = Function; // function
+// let count = BigInt; // big integer
+// let symbol = Symbol; // simbol, takrorlanmas qiymat
+
+
+// let a = 100;
+
+// a = 200;
+
+// console.log(a);
+let a = [];
+let b = {};
+let c = b + a;
+console.log(c)
